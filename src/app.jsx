9e04c81e458cb57a5257d5bc475ac1973ab061e2
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-10-19 16:55:33 -0700

    oh

--- src/app.jsx
+++ src/app.jsx
@@ -2,17 +2,13 @@
 export function App(props) {
   return (
     <>
-      <p>Hello Vite + Preact!</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
+      <p>Cryptex</p>
+
+      <p>A website for me and my bot</p>
+
+      <p>Want to invite it? I sure you do, <a href="https://discord.com/oauth2/authorize?client_id=787927476177076234&permissions=2486562007&scope=bot%20applications.commands" target="_blank">Click here</a></p>
+
+      
     </>
   )
 }
