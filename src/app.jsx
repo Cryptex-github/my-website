73c4371c31133843f7ba197eac6edfadfe02dcef
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-10-19 17:38:05 -0700

    smh grammar fine

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ export function App(props) {
 
       <p>A website for me and my bot</p>
 
-      <p>Want to invite it? I sure you do, <a href="https://discord.com/oauth2/authorize?client_id=787927476177076234&permissions=2486562007&scope=bot%20applications.commands" target="_blank">Click here</a></p>
+      <p>Want to invite it? I'm sure you do, <a href="https://discord.com/oauth2/authorize?client_id=787927476177076234&permissions=2486562007&scope=bot%20applications.commands" target="_blank">Click here</a></p>
 
       <Footer />
     </>
