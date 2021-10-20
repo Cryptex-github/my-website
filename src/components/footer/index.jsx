d2f662b15d627b66bbc3a044fca69f97935b874e
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-10-19 17:55:35 -0700

    o

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,7 +3,7 @@ import style from './style.css';
 export default function Footer() {
     return (
         <footer className={style.footer}>
-            <p>Copyright ©️ Cryptex 2021-present. All rights Reserved.</p>
+            Copyright ©️ Cryptex 2021-present. All rights Reserved.
         </footer>
     );
 }
