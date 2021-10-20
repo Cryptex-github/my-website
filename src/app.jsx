acbd782bf22180d1e985379c992fed64d0ec88f5
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-10-19 17:05:26 -0700

    h google sus

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,13 @@
 export function App(props) {
   return (
     <>
-      <p>Cryptex</p>
+      <h1>Cryptex</h1>
 
       <p>A website for me and my bot</p>
 
       <p>Want to invite it? I sure you do, <a href="https://discord.com/oauth2/authorize?client_id=787927476177076234&permissions=2486562007&scope=bot%20applications.commands" target="_blank">Click here</a></p>
 
-      
+
     </>
   )
 }
