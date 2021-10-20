0c140d42e0b21323c48fbb73b43e865660726011
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-10-19 17:34:04 -0700

    Added footer.

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/footer"
 
 export function App(props) {
   return (
@@ -8,7 +9,7 @@ export function App(props) {
 
       <p>Want to invite it? I sure you do, <a href="https://discord.com/oauth2/authorize?client_id=787927476177076234&permissions=2486562007&scope=bot%20applications.commands" target="_blank">Click here</a></p>
 
-
+      <Footer />
     </>
   )
 }
---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+import style from './style.css';
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>
+            <p>Copyright ©️ Cryptex 2021-present. All rights Reserved.</p>
+        </footer>
+    );
+}
